3fbbda0f9f923804d5c9807aa1eee6077f7c08d1
Aditi Mavalankar <switch2aditi@gmail.com>
2015-10-13 13:18:31 +0530

    phrase raga

--- assign_phrases_to_ragas.py
+++ assign_phrases_to_ragas.py
@@ -1,18 +1,19 @@
 import json
 import csv
 import os
+import ast
 
 svar_map={'S':[0,12],'r':[1,13],'R':[2,14],'g':[3,15],'G':[4,16],'m':[5,17],'M':[6,18],'P':[-5,7,19],'d':[-4,8],'D':[-3,9],'n':[-2,10],'N':[-1,11]}
 single_map={'S':0,'r':1,'R':2,'g':3,'G':4,'m':5,'M':6,'P':7,'d':8,'D':9,'n':10,'N':11}
 svar_index_map={}
 
-input_file=open('all_possible_permutations.json','r')
-phrases=json.load(input_file)
+csvfile=open('difficulty.csv','r')
+phrases=csv.DictReader(csvfile)
 
-input_file=open(os.path.join('../data','nodes.csv'),'r')
+input_file=open('nodes.csv','r')
 nodes=csv.reader(input_file)
 
-input_file=open(os.path.join('../data','edges.csv'),'r')
+input_file=open('edges.csv','r')
 edges=csv.reader(input_file)
 
 ragas=[]
@@ -36,51 +37,53 @@ for i in edges:
 		else:
 			binary_avroh[ragas.index(raga)]=binary_avroh[ragas.index(raga)][:single_map[svar_index_map[svar]]]+'1'+binary_avroh[ragas.index(raga)][single_map[svar_index_map[svar]]+1:]
 
-possible_phrases_aaroh=[]
-possible_phrases_avroh=[]
-raga_id=ragas.index(105)
-
-for k in phrases:
-	print k
-	for i in phrases[k]:
-		flag1=0
-		flag2=0
+for l in ragas:
+	possible_phrases_aaroh=[]
+	possible_phrases_avroh=[]
+	index=1
+	for k in phrases:
+		print k
+		i=ast.literal_eval(k['Phrase'])
+		diff=k['Difficulty']
+		aaroh_exists=0
+		avroh_exists=0
 		for j in i:
 			j=int(j)
 			if j<0:
-				if binary_aaroh[raga_id][j+12]=='0':
-					flag1=1
+				if binary_aaroh[l][j+12]=='0':
+					aaroh_exists=1
 					break
 			elif j>11:
-				if binary_aaroh[raga_id][j-12]=='0':
-					flag1=1
+				if binary_aaroh[l][j-12]=='0':
+					aaroh_exists=1
 					break
 			else:
-				if binary_aaroh[raga_id][j]=='0':
-					flag1=1
+				if binary_aaroh[l][j]=='0':
+					aaroh_exists=1
 					break
-		if flag1==0:
-			possible_phrases_aaroh.append(i)
+		if aaroh_exists==0:
+			possible_phrases_aaroh.append([index,diff])
 		for j in i:
 			j=int(j)
 			if j<0:
-				if binary_avroh[raga_id][j+12]=='0':
-					flag2=1
+				if binary_avroh[l][j+12]=='0':
+					avroh_exists=1
 					break
 			elif j>11:
-				if binary_avroh[raga_id][j-12]=='0':
-					flag2=1
+				if binary_avroh[l][j-12]=='0':
+					avroh_exists=1
 					break
 			else:
-				if binary_avroh[raga_id][j]=='0':
-					flag2=1
+				if binary_avroh[l][j]=='0':
+					avroh_exists=1
 					break
-		if flag2==0:
-			possible_phrases_avroh.append(i)
+		if avroh_exists==0:
+			possible_phrases_avroh.append([index,diff])
+		index=index+1
+	output_file=open(str(l)+'.json','w')
+	output_file.write('Aaroh:\n')
+	json.dump(possible_phrases_aaroh,output_file,indent=2)
+	output_file.write('\nAvroh:\n')
+	json.dump(possible_phrases_avroh,output_file,indent=2)
+	output_file.close()
 	
-output_file=open('raga_phrase_aaroh_avroh_map.json','w')
-output_file.write('Raga: 105\nAaroh: ')
-json.dump(possible_phrases_aaroh,output_file,indent=2)
-output_file.write('\nAvroh: ')
-json.dump(possible_phrases_avroh,output_file,indent=2)
-output_file.close()
